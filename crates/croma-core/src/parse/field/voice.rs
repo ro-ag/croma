b10005ac13f43ac633199e2cce2a2eaf3b860006
Rodrigo Agurto <roagto@gmail.com>
2026-06-07 21:15:27 -0700

    Split misc field parsing into parse/field/misc.rs

--- crates/croma-core/src/parse/field/voice.rs
+++ crates/croma-core/src/parse/field/voice.rs
@@ -2,7 +2,7 @@
 
 use super::*;
 use crate::diagnostic::Span;
-use super::{is_escaped, split_first_word, trim_quoted_value_span, trim_value_span};
+use super::misc::{is_escaped, split_first_word, trim_quoted_value_span, trim_value_span};
 
 pub(super) fn parse_voice(value: Spanned<String>) -> VoiceDefinition {
     let (id, properties) = split_first_word(value);
